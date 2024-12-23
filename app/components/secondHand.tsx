8f900d400d3ca855d2f6c59560f016a13c416d65
Taiga ito <dawoyiteng9@gmail.com>
2024-12-23 09:59:28 +0900

    Animation added.

--- app/components/secondHand.tsx
+++ app/components/secondHand.tsx
@@ -22,7 +22,7 @@ export default function SecondHand({ isDarkMode }: isDarkModeType) {
 
   return (
     <div
-      className={`absolute left-1/2 ${
+      className={`absolute left-1/2 transition-all ${
         isDarkMode ? "bg-black" : "bg-white"
       } w-44  `}
       style={{
