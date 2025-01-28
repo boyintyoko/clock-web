491e1ce1cba03947a90e03fd4c68247dee9104c2
boyintyoko <161582250+boyintyoko@users.noreply.github.com>
2025-01-29 07:37:11 +0900

    多分できる

--- app/api/proxy/[id]/route.ts
+++ app/api/proxy/[id]/route.ts
@@ -1,10 +1,13 @@
-import { NextResponse } from "next/server";
-import axios from "axios";
-import type ImageType from "@/app/types/ImagesType";
+import { NextResponse } from 'next/server';
+import axios from 'axios';
+import type ImageType from '@/app/types/ImagesType';
 
-export async function GET(req: Request, context: { params: { id: string } }) {
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
   try {
-    const { id } = await context.params;
+    const { id } = params;
 
     const response = await axios.get<ImageType[]>(
       `https://api.unsplash.com/photos/random?client_id=${process.env.NEXT_PUBLIC_UNSPLASH_API_END_KEY}&count=${id}`
@@ -14,9 +17,9 @@ export async function GET(req: Request, context: { params: { id: string } }) {
 
     return NextResponse.json(data);
   } catch (err) {
-    console.error("Error fetching API:", err);
+    console.error('Error fetching API:', err);
     return NextResponse.json(
-      { error: "Failed to fetch data" },
+      { error: 'Failed to fetch data' },
       { status: 500 }
     );
   }
