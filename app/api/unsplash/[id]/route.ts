9ca01382556483c8157fcdb9282ce7711967dcd6
Your Name <your.email@example.com>
2025-08-23 16:18:17 +0900

    ゴミeslintのせいでエラー起きた

--- app/api/unsplash/[id]/route.ts
+++ app/api/unsplash/[id]/route.ts
@@ -4,22 +4,22 @@ import type ImageType from "@/app/types/ImagesType";
 
 export async function GET(
   req: Request,
-  context: { params: Promise<{ id: string }> }
+  context: { params: Promise<{ id: string }> },
 ) {
   try {
     const { id } = await context.params;
     const response = await axios.get<ImageType[]>(
-      `https://api.unsplash.com/photos/random?client_id=${process.env.NEXT_PUBLIC_UNSPLASH_API_END_KEY}&count=${id}`
+      `https://api.unsplash.com/photos/random?client_id=${process.env.NEXT_PUBLIC_UNSPLASH_API_END_KEY}&count=${id}`,
     );
 
     const data = response.data;
 
     return NextResponse.json(data);
-  } catch (err) {
+  } catch (err: unknown) {
     console.error("Error fetching API:", err);
-    return NextResponse.json(
-      { error: "Failed to fetch data" },
-      { status: 500 }
-    );
+
+    const message = err instanceof Error ? err.message : "Failed to fetch data";
+
+    return NextResponse.json({ error: message }, { status: 500 });
   }
 }
