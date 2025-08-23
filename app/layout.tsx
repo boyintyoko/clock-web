f380b5a792facbd776fc4cff06567af5ca264cf4
Your Name <your.email@example.com>
2025-08-23 19:26:00 +0900

    fix: bodyにgoogle search consoleのためのmetaタグを追加しました

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,6 +81,10 @@ export default function RootLayout({
         />
         <meta name="apple-mobile-web-app-title" content="MyWebSite" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="google-site-verification"
+          content="r9bgfR9jIhvPVPAHKCoYpVPS3jrkj4baVspn_fISchA"
+        />
         <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body
