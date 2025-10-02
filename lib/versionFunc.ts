b4cf3dc36e520a0bf69ee5dd49eb169b92bb2575
Your Name <your.email@example.com>
2025-10-03 07:35:15 +0900

    fix(versionFunc): ちょっと変更

--- lib/versionFunc.ts
+++ lib/versionFunc.ts
@@ -5,7 +5,7 @@ export default function VersionFunc() {
   const isNowVersion = "1.0.0"
 
   const version = localStorage.getItem("version");
-  if (!version) {
+  if (!version || version !== isNowVersion) {
     localStorage.setItem("version", isNowVersion);
     localStorage.removeItem("urlData");
     localStorage.setItem("urlData", JSON.stringify(urlData))
